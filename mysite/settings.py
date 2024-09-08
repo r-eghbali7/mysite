9b804660392c1c9de32fa3e86a28578ff2279951
emar2001 <70277697+emar2001@users.noreply.github.com>
2024-09-08 16:31:30 +0330

    added static files and directories in settings
    + static root
    + static url
    + media root
    + media url

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "/Users/emar/Documents/GitHub/mysite/website/static"
+STATICFILES_DIRS = [
+    BASE_DIR / "statics"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from website.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,5 @@ urlpatterns = [
     path('', include('website.urls'))
     
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
