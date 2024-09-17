7bc12415b74918a102670eb881b4eedf4748e253
emar2001 <70277697+emar2001@users.noreply.github.com>
2024-09-17 22:59:02 +0330

    created post and contact classes
    used oop and database tables to create classes for posts and contacts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class post(models.Model):
+    # image
+    # author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # tag
+    # category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)  # 2024-09-17 19:02:55.146707
+                                                        # 2024-09-17 19:05:49.694708
+
