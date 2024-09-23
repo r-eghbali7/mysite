8cb19896c51205c244602305f91de754b31ada3b
emar2001 <70277697+emar2001@users.noreply.github.com>
2024-09-23 13:13:08 +0330

    showed posts in a test file
    created test.html and showed posts in it using python scripts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from blog.models import post
 
 # Create your views here.
 
@@ -7,3 +8,8 @@ def blog_view(request):
 
 def blog_single(request):
     return render(request, './blog/blog-single.html')
+
+def test(request):
+    posts = post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'test.html',context)
