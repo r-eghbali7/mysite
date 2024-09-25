96a5a0bbb499793024c19a17c31da63406fa80b3
emar2001 <70277697+emar2001@users.noreply.github.com>
2024-09-26 01:43:13 +0330

    updated views.py
    added a view counter which counts views when blog_view function is called.

--- blog/views.py
+++ blog/views.py
@@ -5,15 +5,21 @@ from django.utils import timezone
 # Create your views here.
 
 def blog_view(request):
-    posts = post.objects.filter(status=1)
+    posts = post.objects.filter(published_date__lt = timezone.now())
     context = {'posts': posts}
+    for post_ in posts:
+       post_.counted_views += 1
+       post_.save()
     return render(request, './blog/blog-home.html',context)
 
 def blog_single(request):
-    return render(request, './blog/blog-single.html')
+    posts = post.objects.filter(published_date__lt = timezone.now())
+    context = {'posts': posts}
+    #post.counted_views += 1
+    return render(request, './blog/blog-single.html', context)
 
 def test(request):
-    Post = post.objects.filter(published_date__gt= timezone.now())
+    Post = post.objects.filter(published_date__lt= timezone.now())
     #Post = post.objects.all()
     context = {'Post' : Post}
     return render(request, 'test.html', context)
