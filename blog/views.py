d005b17d020a5d1db105bb8e7531fffe6cfdabcf
r.eghbali <103234313+r-eghbali7@users.noreply.github.com>
2024-10-16 13:19:46 +0330

    تغییرات برای سایت پست قبل و بعد

--- blog/views.py
+++ blog/views.py
@@ -9,14 +9,43 @@ def blog_view(request):
     context = {'posts': posts}
     return render(request, './blog/blog-home.html',context)
 
-def blog_single(request, pid):
-    Post = get_object_or_404(post, pk=pid, status = 1)
-    context = {'Post':Post, 'pid':pid}
-    Post.counted_views += 1
-    Post.save()
+# def blog_single(request, pid):
+#     Post = get_object_or_404(post, pk=pid, status = 1)
+#     context = {'Post':Post, 'pid':pid}
+#     Post.counted_views += 1
+#     Post.save()
 
-    return render(request, './blog/blog-single.html', context)
+#     return render(request, './blog/blog-single.html', context)
 
 def test(request, pid):
     context = {'pid':pid}
     return render(request, 'test.html', context)
+
+
+
+from django.shortcuts import render, get_object_or_404
+from blog.models import post
+from django.utils import timezone
+
+def blog_single(request, pid):
+    Post = get_object_or_404(post, pk=pid, status=1)
+    
+    # افزایش شمارش بازدید
+    Post.counted_views += 1
+    Post.save()
+
+    # پیدا کردن پست قبلی و بعدی
+    previous_post = post.objects.filter(published_date__lt=Post.published_date, status=1).order_by('-published_date').first()
+    next_post = post.objects.filter(published_date__gt=Post.published_date, status=1).order_by('published_date').first()
+
+
+
+    context = {
+        'Post': Post,
+        'pid': pid,
+        'next_post':next_post,
+        'previous_post':previous_post,
+    }
+        
+
+    return render(request, './blog/blog-single.html', context)
--- mysite/settings.py
+++ mysite/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "/Users/emar/Documents/GitHub/season_4/mysite/website/static"
+STATIC_ROOT = "os.path.join(BASE_DIR, 'static')"
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = "/Users/emar/Documents/GitHub/season_4/mysite/website/media"
+MEDIA_ROOT = "os.path.join(BASE_DIR, 'media')"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
